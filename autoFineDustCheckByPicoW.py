fb0f4986214715828cc27cf948fa4c1ced32046e
Jason Juhyun Kim <mtinet@hanmail.net>
2023-04-08 23:33:46 +0900

    Update autoFineDustCheckByPicoW.py

--- autoFineDustCheckByPicoW.py
+++ autoFineDustCheckByPicoW.py
@@ -6,32 +6,10 @@ import network
 import urequests 
 from timezoneChange import timeOfSeoul # timezoneChange.py 파일이 같은 폴더에 있어야 동작함 
 
-# 자기 정보 넣기(Open Wether Map API Key, 측정하고자 하는 곳의 위도, 경도 정보, 자신이 사용하는 WiFi정보) 
-# https://openweathermap.org/appid 에서 로그인 하고 https://home.openweathermap.org/api_keys 로 이동해서 API Key를 발급받음
-API_KEY = '24109ddecb29a5405afe2a8df42c5e34'
-
-# 확인하고 싶은 위치 정보 입력 
-locations = [
-    ('Seoul', '37.566', '126.9784'),
-    ('San Francisco', '37.77493', '-122.41942'),
-    ('Sevilla', '37.38283', '-5.97317')
-]
 
 # 와이파이 정보 
 SSID = 'U+Net454C'
 password = 'DDAE014478'
-
-
-# 네오픽셀의 셀 갯수, PIO상태, 핀번호 정의 
-numpix = 4
-PIO = 0
-Pin = 0
-
-# 네오픽셀이 RGB타입일 때 네오픽셀 수, PIO상태, 핀번호, 네오픽셀 타입 순으로 선택
-strip = Neopixel(numpix, PIO, Pin, "RGB")
-
-# 밝기 설정(0~255)
-strip.brightness(150)
 
 # 와이파이 연결하기
 wlan = network.WLAN(network.STA_IF)
@@ -48,6 +26,30 @@ else:
     print(wlan.ifconfig())
     print("WiFi is Connected")
     print()
+
+
+# 자기 정보 넣기(Open Wether Map API Key, 측정하고자 하는 곳의 위도, 경도 정보, 자신이 사용하는 WiFi정보) 
+# https://openweathermap.org/appid 에서 로그인 하고 https://home.openweathermap.org/api_keys 로 이동해서 API Key를 발급받음
+API_KEY = '24109ddecb29a5405afe2a8df42c5e34'
+
+# 확인하고 싶은 위치 정보 입력 
+locations = [
+    ('Seoul', '37.566', '126.9784'),
+    ('San Francisco', '37.77493', '-122.41942'),
+    ('Sevilla', '37.38283', '-5.97317')
+]
+
+
+# 네오픽셀의 셀 갯수, PIO상태, 핀번호 정의 
+numpix = 4
+PIO = 0
+Pin = 0
+
+# 네오픽셀이 RGB타입일 때 네오픽셀 수, PIO상태, 핀번호, 네오픽셀 타입 순으로 선택
+strip = Neopixel(numpix, PIO, Pin, "RGB")
+
+# 밝기 설정(0~255)
+strip.brightness(150)
 
     
 def get_air_quality_index(lat, lon, api_key):
@@ -117,4 +119,5 @@ while True:
         
         time.sleep(5)  # 각 위치마다 5초 간격으로 업데이트
         print()
+
 
